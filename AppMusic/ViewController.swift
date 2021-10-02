689467f4650ac684f456c27511b5d11692123f78
ziman <alex.cheranov@yandex.ru>
2021-10-02 21:12:19 +0300

    Initial Commit

---
+++ AppMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMusic
+//
+//  Created by Алексей Черанёв on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

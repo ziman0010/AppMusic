c3d9e20d420019d416034d0fe0063ec243baf7e0
ziman <alex.cheranov@yandex.ru>
2021-10-02 22:28:57 +0300

    initial commit and tab bar

---
+++ AppMusic/MainTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarController.swift
+//  AppMusic
+//
+//  Created by Алексей Черанёв on 02.10.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        let navigationVC = generateViewController(rootViewController: SearchViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-search-5nav-icon"), title: "Search")
+        
+        let libraryVC = generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-library-5nav-icon"), title: "Library")
+        viewControllers = [
+            navigationVC,
+            libraryVC
+        ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        
+        return navigationVC
+    }
+}
--- AppMusic/SearchViewController.swift
+++ AppMusic/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  AppMusic
 //
 //  Created by Алексей Черанёв on 02.10.2021.
@@ -7,14 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
     }
-
-
 }
-

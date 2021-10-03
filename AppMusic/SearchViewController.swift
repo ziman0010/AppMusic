7a59bde68aa45651734f24c0d98814a60d73118e
ziman <alex.cheranov@yandex.ru>
2021-10-03 22:23:10 +0300

    Searchbar + cell

--- AppMusic/SearchViewController.swift
+++ AppMusic/SearchViewController.swift
@@ -7,9 +7,48 @@
 
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var bandName: String
+    
+}
+
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    let tracks = [TrackModel(trackName: "bad guy", bandName: "Billy Eilish"), TrackModel(trackName: "bury a friend", bandName: "Billie Eilish")]
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = " \(track.trackName) \n\(track.bandName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "Image")
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 }
